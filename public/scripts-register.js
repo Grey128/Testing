e82a00ac077a496b83a48629a3a222959baa15b8
Grey256 <jumpykarate@gmail.com>
2023-04-08 20:14:13 -0500

    tests

--- public/scripts-register.js
+++ public/scripts-register.js
@@ -3,11 +3,17 @@ document.getElementById('register-form').addEventListener('submit', async (event
   
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const password2 = document.getElementById('password2').value;
+  
+    if (password !== password2) {
+      alert('Passwords do not match. Please try again.');
+      return;
+    }
   
     const response = await fetch('/register', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'users/json',
       },
       body: JSON.stringify({ username, password }),
     });
