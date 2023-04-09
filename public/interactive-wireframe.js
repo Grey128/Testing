af28ff3f05d506ff6008b3d9475a8636fc56ac86
Grey256 <jumpykarate@gmail.com>
2023-04-09 01:35:43 -0500

    tests 	modified:   public/interactive-wireframe.js 	modified:   public/styles.css

--- public/interactive-wireframe.js
+++ public/interactive-wireframe.js
@@ -5,82 +5,74 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 const points = [];
 const connections = [];
 const gridSize = 50;
 const connectionDistance = 100;
-const interactionRadius 80;
+const interactionRadius = 80;
 
 function createPoints() {
-    for (let x = 0; x < canvas.width; x += gridSize) {
-        for (let y = 0; y < canvas.height; y += gridSize) {
-            points.push(new Point(x, y));
-        }
+  for (let i = 0; i < canvas.width; i += gridSize) {
+    for (let j = 0; j < canvas.height; j += gridSize) {
+      points.push(new Point(i, j));
     }
+  }
 }
 
 function connectPoints() {
-    points.forEach((point, i) => {
-        points.forEach((point2, j) => {
-            if (i !== j) {
-                const dx = point.x - point2.x;
-                const dy = point.y - point2.y;
-                const distance = Math.sqrt(dx * dx + dy * dy);
-
-                if (distance < connectionDistance) {
-                    connections.push([point, point2]);
-                }
-            }
-        });
-    });
+  for (let i = 0; i < points.length; i++) {
+    for (let j = i + 1; j < points.length; j++) {
+      connections.push([points[i], points[j]]);
+    }
+  }
 }
 
 function drawGrid() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    connections.forEach(connection => {
-        ctx.beginPath();
-        ctx.moveTo(connection[0].x, connection[0].y);
-        ctx.lineTo(connection[1].x, connection[1].y);
-        ctx.fillStyle = 'black';
-        ctx.stroke();
-    });
+  connections.forEach(connection => {
+    ctx.beginPath();
+    ctx.moveTo(connection[0].x, connection[0].y);
+    ctx.lineTo(connection[1].x, connection[1].y);
+    ctx.strokeStyle = 'rgba(0, 0, 0, 0.3)';
+    ctx.stroke();
+  });
 
-    points.forEach(point => {
-        ctx.beginPath();
-        ctx.arc(point.x, point.y, 2, 0, Math.PI * 2);
-        ctx.fillStyle = 'black';
-        ctx.fill();
-    });
+  points.forEach(point => {
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+    ctx.fill();
+  });
 }
 
 createPoints();
 connectPoints();
 drawGrid();
 
-canvas.addEventListener('mousemove', (event) => {
-    const cursorPos = new Point(event.clientX, event.clientY);
+canvas.addEventListener('mousemove', e => {
+  const cursorPos = new Point(e.clientX, e.clientY);
 
-    points.forEach(point => {
-        const dx = event.clientX - point.x;
-        const dy = event.clientY - point.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+  points.forEach(point => {
+    const dx = point.x - cursorPos.x;
+    const dy = point.y - cursorPos.y;
+    const dist = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < interactionRadius) {
-            const angle = Math.atan2(dy, dx);
-            const tx = event.clientX - Math.cos(angle) * interactionRadius;
-            const ty = event.clientY - Math.sin(angle) * interactionRadius;
+    if (dist < interactionRadius) {
+      const angle = Math.atan2(dy, dx);
+      const offsetX = Math.cos(angle) * (interactionRadius - dist) * 0.2;
+      const offsetY = Math.sin(angle) * (interactionRadius - dist) * 0.2;
 
-            point.x += tx - point.x;
-            point.y += ty - point.y;
-        }
-    });
+      point.x += offsetX;
+      point.y += offsetY;
+    }
+  });
 
-    drawGrid();
+  drawGrid();
 });
