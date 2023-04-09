906c3b567486969104e7bb7fa078443beba4e6b9
Grey256 <jumpykarate@gmail.com>
2023-04-09 00:49:24 -0500

    tests

--- public/scripts-archive.js
+++ public/scripts-archive.js
@@ -1,54 +1,38 @@
-let stars = [];
-let starIdCounter = 1;
-let mouseX = 0;
-let mouseY = 0;
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body');
+  const starsContainer = document.createElement('div');
+  starsContainer.style.position = 'fixed';
+  starsContainer.style.top = '0';
+  starsContainer.style.left = '0';
+  starsContainer.style.width = '100%';
+  starsContainer.style.height = '100%';
+  starsContainer.style.pointerEvents = 'none';
+  body.appendChild(starsContainer);
 
-function createStar(x, y) {
+  let mouseX = 0;
+  let mouseY = 0;
+  const starFrequency = 10; // 10 stars per second
+
+  body.addEventListener('mousemove', (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+  });
+
+  function createStar(x, y) {
     const star = document.createElement('div');
     star.classList.add('star');
-    star.style.left = x + 'px';
-    star.style.top = y + 'px';
-    star.id = 'star' + starIdCounter;
-    starIdCounter++;
-    stars.push({ element: star, velocity: { x: Math.random() * 2 - 1, y: Math.random() * 2 - 1 } });
-    document.body.appendChild(star);
-}
+    star.style.left = `${x}px`;
+    star.style.top = `${y}px`;
+    starsContainer.appendChild(star);
 
-document.addEventListener('mousemove', (e) => {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-});
+    setTimeout(() => {
+      starsContainer.removeChild(star);
+    }, 1000);
+  }
 
-function spawnStars() {
+  function spawnStar() {
     createStar(mouseX, mouseY);
-}
+  }
 
-setInterval(spawnStars, 1000 / 10); // Spawn stars 10 times per second
-
-function updateStarPositions() {
-    stars.forEach((star, index) => {
-        star.element.style.left = parseFloat(star.element.style.left) + star.velocity.x + 'px';
-        star.element.style.top = parseFloat(star.element.style.top) + star.velocity.y + 'px';
-
-        if (parseFloat(star.element.style.left) < 0 || parseFloat(star.element.style.top) < 0 || parseFloat(star.element.style.left) > window.innerWidth || parseFloat(star.element.style.top) > window.innerHeight) {
-            document.body.removeChild(star.element);
-            stars.splice(index, 1);
-        }
-    });
-}
-
-setInterval(updateStarPositions, 1000 / 60); // Update star positions 60 times per second
-
-const images = document.querySelectorAll('.gallery img');
-const imageName = document.getElementById('image-name');
-
-images.forEach((img) => {
-    img.addEventListener('mouseover', () => {
-        imageName.textContent = img.dataset.name;
-        imageName.style.display = 'block';
-    });
-
-    img.addEventListener('mouseout', () => {
-        imageName.style.display = 'none';
-    });
+  setInterval(spawnStar, 1000 / starFrequency);
 });
