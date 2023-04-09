242f3b10fb1b760d97ff969f3c917df1b7c54378
Grey256 <jumpykarate@gmail.com>
2023-04-09 12:28:14 -0500

    tests

--- public/commissions-background.js
+++ public/commissions-background.js
@@ -1,46 +1,57 @@
 const particles = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  frameRate(30);
-  for (let i = 0; i < 100; i++) {
-    particles.push(new Particle());
-  }
+  createCanvas(window.innerWidth, window.innerHeight);
+  background(51);
 }
 
 function draw() {
-  background(51);
-  particles.forEach((p) => {
-    p.update();
-    p.show();
-  });
+  background(0, 50);
+
+  if (particles.length < 150) {
+    particles.push(new Particle(mouseX, mouseY));
+  }
+
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].show();
+
+    if (particles[i].finished()) {
+      particles.splice(i, 1);
+    }
+  }
 }
 
 class Particle {
-  constructor() {
-    this.pos = createVector(random(width), random(height));
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.maxSpeed = 2;
+  constructor(x, y) {
+    this.pos = createVector(x, y);
     this.prevPos = this.pos.copy();
+    this.vel = p5.Vector.random2D().mult(2);
+    this.acc = createVector(0, 0);
   }
 
   update() {
+    this.prevPos = this.pos.copy();
+
     this.vel.add(this.acc);
-    this.vel.limit(this.maxSpeed);
     this.pos.add(this.vel);
     this.acc.mult(0);
 
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
+    this.vel.limit(5);
   }
 
   show() {
     stroke(255, 50);
     strokeWeight(1);
     line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
-    this.prevPos = this.pos.copy();
+  }
+
+  finished() {
+    return (
+      this.pos.x < 0 ||
+      this.pos.x > width ||
+      this.pos.y < 0 ||
+      this.pos.y > height
+    );
   }
 }
